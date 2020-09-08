fae96de18f5636057f99c05c409c2cbac1209014
luyen <luyen2011@gmail.com>
2020-09-08 18:49:41 +0700

    up bai

---
+++ module_2/src/introduction_in_java_01/thuc_hanh/HienThiThoiGianHeThong.java
@@ -0,0 +1,11 @@
+package introduction_in_java_01.thuc_hanh;
+
+import java.util.Date;
+
+public class HienThiThoiGianHeThong {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println("Date now : " + now);
+
+    }
+}
