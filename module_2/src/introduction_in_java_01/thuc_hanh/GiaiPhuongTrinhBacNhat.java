c884957020befc47324c6bed1aee88e9a99f93c8
luyen <luyen2011@gmail.com>
2020-09-09 08:19:11 +0700

    up bai

---
+++ module_2/src/introduction_in_java_01/thuc_hanh/GiaiPhuongTrinhBacNhat.java
@@ -0,0 +1,26 @@
+package introduction_in_java_01.thuc_hanh;
+
+import java.util.Scanner;
+
+public class GiaiPhuongTrinhBacNhat {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a,b,c;
+        System.out.print("a : ");
+        a = sc.nextDouble();
+        System.out.print("b : ");
+        b = sc.nextDouble();
+        System.out.print("c : ");
+        c = sc.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
---
+++ module_2/src/introduction_in_java_01/thuc_hanh/SuDungToanTu.java
@@ -0,0 +1,18 @@
+package introduction_in_java_01.thuc_hanh;
+
+import java.util.Scanner;
+
+public class SuDungToanTu {
+    public static void main(String[] args){
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float area = width * height;
+        System.out.println("Area is : " + area);
+
+    }
+}
