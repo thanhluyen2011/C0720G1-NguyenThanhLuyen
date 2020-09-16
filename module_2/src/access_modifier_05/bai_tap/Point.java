fd5788febc7ab0c9fcb08a18e7354920c2c85b08
luyen <luyen2011@gmail.com>
2020-09-16 08:50:20 +0700

    up bai

--- module_2/src/access_modifier_05/bai_tap/Point.java
+++ module_2/src/access_modifier_05/bai_tap/Point.java
@@ -1,8 +1,10 @@
 package access_modifier_05.bai_tap;
 
+import java.util.Arrays;
+
 public class Point {
-    private float x;
-    private float y;
+    public float x;
+    public float y;
     public Point () {
     }
     public Point (float x,float y) {
@@ -35,5 +37,68 @@ public class Point {
         array[1]=getY();
         return array;
     }
+    public String toString(){
+        return getX() + "x\t" +
+                getY() +  "y";
+    }
 }
-class MoveablePoint extends 
+class MoveablePoint extends Point{
+    private float xSpeed;
+    private float ySpeed;
+    public MoveablePoint(){}
+    public MoveablePoint(float x,float y,float xSpeed,float ySpeed){
+        super(x,y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public MoveablePoint(float xSpeed, float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    private void setSpeed(float xSpeed, float ySpeed){
+        this.xSpeed = getXSpeed();
+        this.ySpeed = getYSpeed();
+    }
+    private float[] getSpeed(){
+        float[] array = new float[2];
+        array[0] = getXSpeed();
+        array[1] = getYSpeed();
+        return array;
+    }
+    public String toString(){
+        return super.toString() +"\t" +
+                xSpeed + "xSpeed \t" +
+                ySpeed + "ySpeed \t" +
+                Arrays.toString(getSpeed()) + "getSpeed";
+    }
+    public float[] move(){
+        this.x += getXSpeed();
+        this.y += getYSpeed();
+        return getXY();
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        Point point = new Point(1,2);
+        MoveablePoint moveablePoint = new MoveablePoint(1,2,3,3);
+        System.out.println(point.toString());
+        System.out.println(moveablePoint.toString());
+        System.out.println(Arrays.toString(moveablePoint.move()));
+    }
+}
---
+++ module_2/src/access_modifier_05/bai_tap/Shape.java
@@ -0,0 +1,8 @@
+package access_modifier_05.bai_tap;
+
+public class Shape {
+    private double side1 = 1.0;
+    private double side2 = 1.0;
+    private double side3 = 1.0;
+    
+}
