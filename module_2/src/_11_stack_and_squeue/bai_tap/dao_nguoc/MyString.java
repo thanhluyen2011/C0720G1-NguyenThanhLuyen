1791d5661552b7fc28ef5940391610bdc5cc60e4
luyen <luyen2011@gmail.com>
2020-09-23 09:33:27 +0700

    up bai

--- module_2/src/_11_stack_and_squeue/bai_tap/dao_nguoc/MyString.java
+++ module_2/src/_11_stack_and_squeue/bai_tap/dao_nguoc/MyString.java
@@ -1,21 +1,36 @@
 package _11_stack_and_squeue.bai_tap.dao_nguoc;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class MyString {
     public static void main(String[] args) {
-        String mWork = "thanh luyen";
-        Stack<Character> listStack = new Stack<>();
-        for (int i = 0; i < mWork.length(); i++) {
-            listStack.push(mWork.charAt(i));
+        Stack<String> stringStack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập chuỗi");
+        String[] mWord = scanner.nextLine().split(" ");
+        for (int i = 0; i < mWord.length; i++) {
+            stringStack.push(mWord[i]);
         }
-        System.out.println(listStack);
-        Stack<Character> temp = new Stack<>();
-        while (!listStack.isEmpty()){
-            temp.push(listStack.pop());
+        Stack<String> stringStackTemp=new Stack<>();
+        System.out.println("trước" + stringStack);
+        while (!stringStack.isEmpty()) {
+            stringStackTemp.push(stringStack.pop());
         }
-        listStack = temp;
-        System.out.println(listStack);
+        stringStack=stringStackTemp;
+        System.out.println("sau"+stringStack);
+//        Stack<Character> listStack = new Stack<>();
+//        for (int i = 0; i < mWork.length(); i++) {
+//            listStack.push(mWork.charAt(i));
+//        }
+//        System.out.println(listStack);
+//        Stack<Character> temp = new Stack<>();
+//        while (!listStack.isEmpty()){
+//            temp.push(listStack.pop());
+//        }
+//        listStack = temp;
+//        System.out.println(listStack);
 
     }
 }
