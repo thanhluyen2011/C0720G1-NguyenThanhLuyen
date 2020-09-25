ca2e4867e5c436c26054e5b2084946dfac843a8f
luyen <luyen2011@gmail.com>
2020-09-25 08:58:03 +0700

    up bai

--- module_2/src/_12_java_collection_framework/bai_tap/DescendingOrder.java
+++ module_2/src/_12_java_collection_framework/bai_tap/DescendingOrder.java
@@ -5,13 +5,13 @@ import java.util.Comparator;
 public class DescendingOrder implements Comparator<Product> {
     @Override
     public int compare(Product t1, Product t2) {
-        double res = t1.getPrice()-t2.getPrice();
-        if (res>0){
-            return -1;
-        }else if (res<0){
-            return 1;
-        }
-        return 0;
+        return t2.getPrice()-t1.getPrice();
+//        if (res>0){
+//            return -1;
+//        }else if (res<0){
+//            return 1;
+//        }
+//        return 0;
     }
 
 }
--- module_2/src/_12_java_collection_framework/bai_tap/SortInCreasePrice.java
+++ module_2/src/_12_java_collection_framework/bai_tap/SortInCreasePrice.java
@@ -5,12 +5,12 @@ import java.util.Comparator;
 public class SortInCreasePrice implements Comparator<Product> {
     @Override
     public int compare(Product t1, Product t2) {
-        double res = t2.getPrice()-t1.getPrice();
-        if (res>0){
-            return -1;
-        }else if (res<0){
-            return 1;
-        }
-        return 0;
+        return t1.getPrice()-t2.getPrice();
+//        if (res>0){
+//            return -1;
+//        }else if (res<0){
+//            return 1;
+//        }
+//        return 0;
     }
 }
