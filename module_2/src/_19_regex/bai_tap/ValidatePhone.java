99d6bf8b64e657fcee84388b33d973f74561c865
luyen <luyen2011@gmail.com>
2020-10-06 08:48:51 +0700

    up bai

--- module_2/src/_19_regex/bai_tap/ValidatePhone.java
+++ module_2/src/_19_regex/bai_tap/ValidatePhone.java
@@ -7,18 +7,19 @@ import java.util.regex.Pattern;
 public class ValidatePhone {
     public static void main(String[] args) {
         final String REGEX = "^\\(\\d{2}\\)[-]\\([0]\\d{9}\\)$";
-//        final String REGEX ="^[(]{1}\\d{2}[)]{1}[-]{1}[(]{1}[0]{1}\\d{9}[)]{1}$";
+//        final String REGEX ="^[(]{1}\\d{2}[)]{1}[-]{1}[(]{1}[0]{1}\\d{9}[)]{1}$"; Cường
+//        final String REGEX = "^\\(\\d{2}\\)-\\(0\\d{9}\\)$"; Nguyễn Khoa
         Scanner sc = new Scanner(System.in);
         Pattern pattern = Pattern.compile(REGEX);
         while (true){
-            System.out.println("Nhập");
+            System.out.println("Enter");
             String string = sc.nextLine();
             Matcher matcher = pattern.matcher(string);
             if (matcher.find()){
-                System.out.println("Đúng");
+                System.out.println("True");
                 break;
             }else {
-                System.out.println("Sai");
+                System.out.println("Falsef");
             }
         }
 
